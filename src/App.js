87241725a75bd357906b4148ed9c9a5cbf738b8e
LuisTheDeveloper <sprintlmaga@gmail.com>
2022-03-12 22:28:17 +0000

    Home page built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="app">
-      <h1>Test</h1>
+      <Header/>
+      <Home/>
     </div>
   );
 }
