a5a04b1e64ad641f9be007de36700f9d1d5cda47
LuisTheDeveloper <sprintlmaga@gmail.com>
2022-03-13 17:05:44 +0000

    eslint formatting & ecma version & rules

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyABE8CLFY5asAnTr9MRjTCq9LB-eTv7fBk",
-    authDomain: "clone-181b3.firebaseapp.com",
-    projectId: "clone-181b3",
-    storageBucket: "clone-181b3.appspot.com",
-    messagingSenderId: "334650338840",
-    appId: "1:334650338840:web:b6484c4a2fd70a85601d6d",
-    measurementId: "G-F8ZJB37FTF"
-  };
+	apiKey: "AIzaSyABE8CLFY5asAnTr9MRjTCq9LB-eTv7fBk",
+	authDomain: "clone-181b3.firebaseapp.com",
+	projectId: "clone-181b3",
+	storageBucket: "clone-181b3.appspot.com",
+	messagingSenderId: "334650338840",
+	appId: "1:334650338840:web:b6484c4a2fd70a85601d6d",
+	measurementId: "G-F8ZJB37FTF"
+};
