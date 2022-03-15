f1ace0e87f17f87a97bad498a95034c5596c716c
LuisTheDeveloper <sprintlmaga@gmail.com>
2022-03-15 21:27:11 +0000

    Added user firebase authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import {initializeApp} from 'firebase/app'
+import { getAuth, onAuthStateChanged  } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyABE8CLFY5asAnTr9MRjTCq9LB-eTv7fBk",
@@ -8,3 +12,10 @@ const firebaseConfig = {
 	appId: "1:334650338840:web:b6484c4a2fd70a85601d6d",
 	measurementId: "G-F8ZJB37FTF"
 };
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const db = getFirestore(firebaseApp)
+const auth = getAuth()
+const authState = onAuthStateChanged
+export { onAuthStateChanged, db, auth, authState }
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import { BrowserRouter, HashRouter, Outlet, Route, Routes } from 'react-router-dom';
+import React, {useEffect} from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Checkout from '../components/Checkout';
-
 import Header from '../components/Header';
 import Home from '../components/Home';
 import Login from '../components/Login';
+import { auth, onAuthStateChanged } from '../firebase';
+import { useStateValue } from '../StateProvider';
 
 export const AppRouter = () => {
+	const [{}, dispatch] = useStateValue()
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				dispatch({
+					type: 'SET_USER',
+					user: user 
+				})
+			} else {
+				dispatch({
+					type: 'SET_USER',
+					user: null
+				})
+			}
+		})
+	
+	},[])
+
 	return (
 		<BrowserRouter>
 			<Routes>
